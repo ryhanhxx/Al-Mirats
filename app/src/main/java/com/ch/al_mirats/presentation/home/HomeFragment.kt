1a1c9a3c3124c7edb70aba9eb3af90c66aed0d29
ryhanhxx <rayhan328967@gmail.com>
2024-07-07 22:14:26 +0700

    Update font

--- app/src/main/java/com/ch/al_mirats/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/ch/al_mirats/presentation/home/HomeFragment.kt
@@ -54,7 +54,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
