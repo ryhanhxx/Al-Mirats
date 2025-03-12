ca474ac37c0ba42abf801b8a4dfd92d85dc6a1af
Rayhan <rayhan328967@gmail.com>
2025-03-12 09:36:26 +0700

    update onboard

--- app/src/main/java/com/ch/al_mirats/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/ch/al_mirats/presentation/splash/SplashScreen.kt
@@ -6,25 +6,37 @@ import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.ch.al_mirats.R
 import com.ch.al_mirats.databinding.ActivitySplashScreenBinding
+import com.ch.al_mirats.presentation.about.AboutActivity
 import com.ch.al_mirats.presentation.main.MainActivity
+import com.ch.al_mirats.presentation.settings.SettingsDialogFragment
 
 class SplashScreen : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
 
-    private val SPLASH_TIME_OUT: Long = 1000 // Durasi splash screen dalam milidetik (3 detik)
+//    private val SPLASH_TIME_OUT: Long = 1000 // Durasi splash screen dalam milidetik (3 detik)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
-            // Aksi yang akan dilakukan setelah splash screen selesai
-            val intent = Intent(
-                this,
-                MainActivity::class.java
-            ) // Ganti dengan activity tujuan setelah splash screen
+//        Handler().postDelayed({
+//            // Aksi yang akan dilakukan setelah splash screen selesai
+//            val intent = Intent(
+//                this,
+//                MainActivity::class.java
+//            ) // Ganti dengan activity tujuan setelah splash screen
+//            startActivity(intent)
+//            finish()
+//        }, SPLASH_TIME_OUT)
+
+        navigateToHome()
+
+    }
+
+    private fun navigateToHome(){
+        binding.ivStart.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
-            finish()
-        }, SPLASH_TIME_OUT)
+        }
     }
 }
